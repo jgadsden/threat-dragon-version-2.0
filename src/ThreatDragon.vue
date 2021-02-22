064102d1da4583472ab68c5b99741309fcaa3085
Jon Gadsden <jon.gadsden@forgerock.com>
2021-02-22 16:07:23 +0000

    initial threat dragon welcome page

---
+++ src/ThreatDragon.vue
@@ -0,0 +1,19 @@
+<template>
+  <img alt="Vue logo" src="./assets/images/threatdragonx256.png" />
+  <Welcome msg="OWASP Threat Dragon" />
+</template>
+
+<script setup>
+  import Welcome from './components/Welcome.vue'
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    <a href="https://threatdragon.github.io/" target="_blank">Threat Dragon Documentation</a> |
+    <a href="https://owasp.org/www-project-threat-dragon/" target="_blank">OWASP project page</a>
+  </p>
+
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
