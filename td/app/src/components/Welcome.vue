3710469d65bc3ce7c39a88bf71a7443e3c74ab7b
Jon Gadsden <jon.gadsden@forgerock.com>
2021-02-26 07:39:38 +0000

    use correct Vue format

--- td/app/src/components/Welcome.vue
+++ td/app/src/components/Welcome.vue
@@ -1,12 +1,13 @@
 <template>
-  <img alt="Vue logo" src="../assets/images/threatdragonx256.png" />
-  <h1 v-once>{{ msg }} {{ version }}</h1>
+  <div>
+    <img alt="Vue logo" src="../assets/images/threatdragonx256.png" />
+    <h1 v-once>{{ msg }} {{ version }}</h1>
 
-  <p>
-    <a href="https://threatdragon.github.io/" target="_blank">Threat Dragon Documentation</a> |
-    <a href="https://owasp.org/www-project-threat-dragon/" target="_blank">OWASP project page</a>
-  </p>
-
+    <p>
+      <a href="https://threatdragon.github.io/" target="_blank">Threat Dragon Documentation</a> |
+      <a href="https://owasp.org/www-project-threat-dragon/" target="_blank">OWASP project page</a>
+    </p>
+  </div>
 </template>
 
 <script setup>
